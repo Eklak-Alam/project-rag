00ed99527f8ad48327d3e45f21be3d16c618195e
Eklak Alam <167274884+Eklak-Alam@users.noreply.github.com>
2026-08-22 09:07:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from dotenv import load_dotenv
-from rag_engine import process_uploaded_pdf, get_response_stream 123456789
+from rag_engine import process_uploaded_pdf, get_response_stream 12345678
 
 load_dotenv()
 
